07b2bafb292e8f3a04a9914f126f20fdfe0d8a35
CianoDanilo <danilo.ciano@pagopa.it>
2025-05-15 11:43:32 +0200

    feat(keyvault): add RegisterReflectionForBinding annotation

--- src/main/kotlin/it/pagopa/touchpoint/jwtissuerservice/JwtIssuerServiceApplication.kt
+++ src/main/kotlin/it/pagopa/touchpoint/jwtissuerservice/JwtIssuerServiceApplication.kt
@@ -1,13 +1,17 @@
 package it.pagopa.touchpoint.jwtissuerservice
 
+import com.azure.security.keyvault.secrets.implementation.models.SecretAttributes
+import com.azure.security.keyvault.secrets.implementation.models.SecretBundle
 import it.pagopa.touchpoint.jwtissuerservice.config.properties.AzureSecretConfigProperties
 import it.pagopa.touchpoint.jwtissuerservice.config.properties.CacheConfigProperties
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import reactor.core.publisher.Hooks
 
 @SpringBootApplication
+@RegisterReflectionForBinding(SecretBundle::class, SecretAttributes::class)
 @EnableConfigurationProperties(AzureSecretConfigProperties::class, CacheConfigProperties::class)
 class JwtIssuerServiceApplication
 
