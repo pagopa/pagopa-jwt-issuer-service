1457d9d0fb273918bdbb4161010057c0caf05e1a
CianoDanilo <danilo.ciano@pagopa.it>
2025-05-15 14:34:44 +0200

    feat(keyvault): remove test bean

--- src/main/kotlin/it/pagopa/touchpoint/jwtissuerservice/config/AzureConfig.kt
+++ src/main/kotlin/it/pagopa/touchpoint/jwtissuerservice/config/AzureConfig.kt
@@ -3,10 +3,7 @@ package it.pagopa.touchpoint.jwtissuerservice.config
 import com.azure.identity.DefaultAzureCredentialBuilder
 import com.azure.security.keyvault.secrets.SecretAsyncClient
 import com.azure.security.keyvault.secrets.SecretClientBuilder
-import it.pagopa.touchpoint.jwtissuerservice.services.ReactiveAzureKVSecurityKeysService
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.context.event.ApplicationReadyEvent
-import org.springframework.context.ApplicationListener
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -21,17 +18,5 @@ class AzureConfig {
             .vaultUrl(azureKeyVaultEndpoint)
             .credential(DefaultAzureCredentialBuilder().build())
             .buildAsyncClient()
-    }
-
-    @Bean
-    fun basicsApplicationListener(
-        jwtKeysService: ReactiveAzureKVSecurityKeysService
-    ): ApplicationListener<ApplicationReadyEvent> {
-        return ApplicationListener {
-            println(jwtKeysService.getSecret().map { println(it.value) }.block())
-            println(jwtKeysService.getPublic().block())
-            println(jwtKeysService.getPrivate().block()?.format)
-            println(jwtKeysService.getPrivate().block()?.algorithm)
-        }
     }
 }
