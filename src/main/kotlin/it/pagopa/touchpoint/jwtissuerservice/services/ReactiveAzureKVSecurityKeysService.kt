5848e04b33f161f616391dbd0246de1ae5a4d879
CianoDanilo <danilo.ciano@pagopa.it>
2025-05-20 15:14:06 +0200

    [CHK-4130] feat(keyvault): add keyvault service and keystore caching (#8)
    * chore: minor fixes to docker files
    
    * feat: openapi + code generation
    
    * feat: basic controller + service
    
    * wip: basic implementation
    
    * wip: basic implementation
    
    * feat(keyvault): add azure keyvault client and service
    
    * feat(keyvault): add cache
    
    * feat(keyvault): update keyclient
    
    * feat(keyvault): add certificate handling and caching mechanism
    
    * feat(keyvault): add junit tests
    
    * feat(keyvault): run spotless apply
    
    * feat(keyvault): add interface
    
    * feat(keyvault): add helm values
    
    * feat(keyvault): add helm values
    
    * feat(keyvault): add native buildArgs
    
    * feat(keyvault): add RegisterReflectionForBinding annotation
    
    * feat(keyvault): add reflect-config file
    
    * feat(keyvault): update README file and .env.example
    
    * feat(keyvault): add verification-metadata.xml
    
    * feat(keyvault): update tests and reflect-config.json
    
    * feat(keyvault): update reflect-config.json
    
    * feat(keyvault): update reflect-config.json
    
    * feat(keyvault): remove test bean
    
    * feat(keyvault): rename test class
    
    * feat(keyvault): wip
    
    * feat(keyvault): wip
    
    * feat(keyvault): add retry to azure keyvault client config
    
    * feat(keyvault): update tests
    
    * feat(keyvault): spotless apply
    
    * feat(keyvault): update tests
    
    * feat(keyvault): fix sonar issues
    
    * feat(keyvault): update property name
    
    * feat(keyvault): update property name
    
    * feat(keyvault): spotless apply
    
    * feat(keyvault): update cache entry
    
    * feat(keyvault): update cache entry
    
    ---------
    
    Co-authored-by: Pietro Tota <pietro.tota@pagopa.it>

---
+++ src/main/kotlin/it/pagopa/touchpoint/jwtissuerservice/services/ReactiveAzureKVSecurityKeysService.kt
@@ -0,0 +1,66 @@
+package it.pagopa.touchpoint.jwtissuerservice.services
+
+import com.azure.security.keyvault.certificates.CertificateAsyncClient
+import com.azure.security.keyvault.certificates.models.KeyVaultCertificate
+import com.azure.security.keyvault.secrets.SecretAsyncClient
+import com.azure.security.keyvault.secrets.models.KeyVaultSecret
+import it.pagopa.touchpoint.jwtissuerservice.config.properties.AzureSecretConfigProperties
+import java.io.ByteArrayInputStream
+import java.security.KeyStore
+import java.security.PrivateKey
+import java.security.PublicKey
+import java.security.cert.CertificateFactory
+import java.security.cert.X509Certificate
+import java.util.*
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+@Component
+class ReactiveAzureKVSecurityKeysService(
+    private val secretClient: SecretAsyncClient,
+    private val certClient: CertificateAsyncClient,
+    private val azureSecretConfig: AzureSecretConfigProperties,
+) : IReactiveSecurityKeysService {
+    private val keystore = KeyStore.getInstance("PKCS12")
+    private val certFactory = CertificateFactory.getInstance("X.509")
+
+    fun getSecret(): Mono<KeyVaultSecret> {
+        return secretClient.getSecret(azureSecretConfig.name)
+    }
+
+    fun getCerts(): Flux<KeyVaultCertificate> {
+        return certClient
+            .listPropertiesOfCertificateVersions(azureSecretConfig.name)
+            .filter { it.isEnabled }
+            .flatMap { certClient.getCertificateVersion(azureSecretConfig.name, it.version) }
+    }
+
+    fun getKeyStore(): Mono<KeyStore> {
+        return this.getSecret().map {
+            val decodedPfx = Base64.getDecoder().decode(it.value)
+            keystore.load(
+                ByteArrayInputStream(decodedPfx),
+                azureSecretConfig.password.toCharArray(),
+            )
+            keystore
+        }
+    }
+
+    @Cacheable("keyStore", key = "'privateKey'")
+    override fun getPrivate(): Mono<PrivateKey> {
+        return this.getKeyStore().map {
+            it.getKey(it.aliases().nextElement(), azureSecretConfig.password.toCharArray())
+                as PrivateKey
+        }
+    }
+
+    override fun getPublic(): Flux<PublicKey> {
+        return this.getCerts().map {
+            val x509Cert: X509Certificate =
+                certFactory.generateCertificate(ByteArrayInputStream(it.cer)) as X509Certificate
+            x509Cert.publicKey
+        }
+    }
+}
