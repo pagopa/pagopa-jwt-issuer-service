9dad43b1d43e26cf730f18611fe99e0bd0e4e323
CianoDanilo <danilo.ciano@pagopa.it>
2025-05-28 16:23:49 +0200

    wip

--- src/main/kotlin/it/pagopa/touchpoint/jwtissuerservice/controllers/WellKnownController.kt
+++ src/main/kotlin/it/pagopa/touchpoint/jwtissuerservice/controllers/WellKnownController.kt
@@ -2,12 +2,16 @@ package it.pagopa.touchpoint.jwtissuerservice.controllers
 
 import it.pagopa.generated.touchpoint.jwtissuerservice.v1.api.WellKnownApi
 import it.pagopa.generated.touchpoint.jwtissuerservice.v1.model.OpenIDDiscoveryResponseDto
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class WellKnownController() : WellKnownApi {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     override suspend fun getOpenidInfo(): ResponseEntity<OpenIDDiscoveryResponseDto> {
+        logger.info("Getting OpenID Info")
         return ResponseEntity.ok(
             OpenIDDiscoveryResponseDto(
                 jwksUri =
