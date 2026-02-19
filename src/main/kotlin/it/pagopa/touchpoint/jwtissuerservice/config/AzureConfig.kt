c8cf3857d686fe28d7ea917902dc8c309ff79809
Dylan Tangredi <dylan.tangredi.ext@jakala.com>
2026-02-19 16:34:38 +0100

    feat: [PIDM-1537] add mock-credentials option to bypass DefaultAzureCredential

--- src/main/kotlin/it/pagopa/touchpoint/jwtissuerservice/config/AzureConfig.kt
+++ src/main/kotlin/it/pagopa/touchpoint/jwtissuerservice/config/AzureConfig.kt
@@ -1,5 +1,8 @@
 package it.pagopa.touchpoint.jwtissuerservice.config
 
+import com.azure.core.credential.AccessToken
+import com.azure.core.credential.TokenCredential
+import com.azure.core.credential.TokenRequestContext
 import com.azure.core.http.policy.FixedDelay
 import com.azure.core.http.policy.RetryPolicy
 import com.azure.identity.DefaultAzureCredentialBuilder
@@ -9,14 +12,29 @@ import com.azure.security.keyvault.secrets.SecretAsyncClient
 import com.azure.security.keyvault.secrets.SecretClientBuilder
 import it.pagopa.touchpoint.jwtissuerservice.config.properties.AzureKeyVaultClientConfigProperties
 import java.time.Duration
+import java.time.OffsetDateTime
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import reactor.core.publisher.Mono
 
 @Configuration
 class AzureConfig(
     @Autowired private val azureKeyVaultClientConfigProperties: AzureKeyVaultClientConfigProperties
 ) {
+
+    /**
+     * This returns the mock credentials if the env var is set to true (safe default to false) for integration tests,
+     * in which case we use TokenCredential to generate a static dummy token (there is no validation in the akv-emulator)
+     */
+    private fun buildCredential(): TokenCredential {
+        if (azureKeyVaultClientConfigProperties.mockCredentials) {
+            return TokenCredential { _: TokenRequestContext ->
+                Mono.just(AccessToken("mock-token", OffsetDateTime.now().plusHours(1)))
+            }
+        }
+        return DefaultAzureCredentialBuilder().build()
+    }
 
     @Bean
     fun azureKeyVaultSecretClient(): SecretAsyncClient {
@@ -32,7 +50,7 @@ class AzureConfig(
                     )
                 )
             )
-            .credential(DefaultAzureCredentialBuilder().build())
+            .credential(buildCredential())
             .buildAsyncClient()
     }
 
@@ -50,7 +68,7 @@ class AzureConfig(
                     )
                 )
             )
-            .credential(DefaultAzureCredentialBuilder().build())
+            .credential(buildCredential())
             .buildAsyncClient()
     }
 }
