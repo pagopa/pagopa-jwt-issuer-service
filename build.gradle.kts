ca1a474ac553f68a14ab1841f02eaff6bb7136b6
ciuffagianluca <gianluca.ciuffa@pagopa.it>
2026-08-31 16:50:06 +0200

    fix gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -101,7 +101,6 @@ dependencies {
   testImplementation("org.bouncycastle:bcpkix-jdk18on:${Deps.bouncyCastleVersion}")
   testImplementation("org.bouncycastle:bcprov-jdk18on:${Deps.bouncyCastleVersion}")
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-  implementation(kotlin("stdlib-jdk8"))
 }
 
 // openapi code generation
